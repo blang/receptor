99be41e31e098b806695650189e6c596e99b20cb
Benedikt Lang <github@benediktlang.de>
2014-08-03 13:13:07 +0200

    Handler closeCh to doneCh

--- handler/handler.go
+++ handler/handler.go
@@ -6,11 +6,11 @@ import (
 
 type Handler interface {
 	// Started in seperate go routine
-	Handle(eventCh chan event.Event, closeCh chan struct{})
+	Handle(eventCh chan event.Event, doneCh chan struct{})
 }
 
-type HandlerFunc func(eventCh chan event.Event, closeCh chan struct{})
+type HandlerFunc func(eventCh chan event.Event, doneCh chan struct{})
 
-func (f HandlerFunc) Handle(eventCh chan event.Event, closeCh chan struct{}) {
-	f(eventCh, closeCh)
+func (f HandlerFunc) Handle(eventCh chan event.Event, doneCh chan struct{}) {
+	f(eventCh, doneCh)
 }
