12197ae2a54cb7d04ac188f5339a0376a8609297
Benedikt Lang <github@benediktlang.de>
2014-08-04 23:41:46 +0200

    Add RestAPIServer Watcher

---
+++ discovery/restapiserver/restapi.go
@@ -0,0 +1,104 @@
+package restapiserver
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/blang/receptor/discovery"
+	"github.com/blang/receptor/event"
+	"github.com/blang/receptor/handler"
+	"net/http"
+)
+
+type RestAPIServerWatcher struct {
+	Listen string
+	Server *http.Server
+	Router *http.ServeMux
+	//TODO: Add Mutex
+	IsRunning bool
+}
+
+type Config struct {
+	Listen string `json:"listen"`
+}
+
+type ServiceConfig struct {
+	Service string `json:"service"`
+}
+
+func init() {
+	discovery.Watchers["restapiserver"] = &RestAPIServerWatcher{}
+}
+
+func (w *RestAPIServerWatcher) Setup(cfgData json.RawMessage) error {
+	conf := Config{
+		Listen: "127.0.0.1:8080",
+	}
+	err := json.Unmarshal(cfgData, &conf)
+	if err != nil {
+		return err
+	}
+	w.Router = http.NewServeMux()
+	w.Server = &http.Server{Addr: conf.Listen, Handler: w.Router}
+	return nil
+}
+
+type RestEvent struct {
+	Name string `json:"name"`
+	Type string `json:"type"`
+	Host string `json:"host"`
+	Port int    `json:"port"`
+}
+
+func (w *RestAPIServerWatcher) Accept(cfgData json.RawMessage) (handler.Handler, error) {
+	conf := ServiceConfig{
+		Service: "default",
+	}
+	err := json.Unmarshal(cfgData, &conf)
+	if err != nil {
+		return nil, err
+	}
+
+	return handler.HandlerFunc(func(eventCh chan event.Event, closeCh chan struct{}) {
+
+		w.Router.HandleFunc("/service/"+conf.Service, func(w http.ResponseWriter, r *http.Request) {
+			var restEvent RestEvent
+			err := json.NewDecoder(r.Body).Decode(&restEvent)
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				fmt.Fprintf(w, "Bad request: %s", err)
+				return
+			}
+
+			var eventType event.EventType
+			switch restEvent.Type {
+			case "nodeup":
+				eventType = event.EventNodeUp
+			case "nodedown":
+				eventType = event.EventNodeDown
+			default:
+				w.WriteHeader(http.StatusBadRequest)
+				fmt.Fprintln(w, "Bad request: Invalid event type")
+				return
+			}
+
+			eventCh <- &event.SingleNode{
+				EName: restEvent.Name,
+				EHost: restEvent.Host,
+				EPort: restEvent.Port,
+				EType: eventType,
+			}
+
+			fmt.Fprintln(w, "OK")
+		})
+		go func() {
+			if !w.IsRunning {
+				w.IsRunning = true
+				go func() {
+					w.Server.ListenAndServe() // Log error
+				}()
+			}
+			<-closeCh
+			close(eventCh)
+		}()
+	}), nil
+}
---
+++ discovery/restapiserver/restapi_test.go
@@ -0,0 +1,94 @@
+package restapiserver
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/blang/receptor/discovery"
+	"github.com/blang/receptor/event"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestFunc(t *testing.T) {
+	watcher := discovery.Watchers["restapiserver"].(*RestAPIServerWatcher)
+	cfg := &Config{
+		Listen: "127.0.0.1:9991",
+	}
+	b, err := json.Marshal(cfg)
+	if err != nil {
+		t.Fatalf("Could not marshal cfg: %s", string(b))
+	}
+
+	err = watcher.Setup(json.RawMessage(b))
+	if err != nil {
+		t.Fatalf("Watcher setup failed: %s", err)
+	}
+
+	serviceConfig := &ServiceConfig{
+		Service: "testservice",
+	}
+	b, err = json.Marshal(serviceConfig)
+	if err != nil {
+		t.Fatalf("Could not marshal service cfg: %s", string(b))
+	}
+
+	handle, err := watcher.Accept(json.RawMessage(b))
+	if err != nil {
+		t.Fatalf("Watcher accept failed: %s", err)
+	}
+
+	eventCh := make(chan event.Event, 1)
+	closeCh := make(chan struct{})
+
+	watcher.IsRunning = true // Fake Running server
+	testserver := httptest.NewServer(watcher.Router)
+	defer testserver.Close()
+	go handle.Handle(eventCh, closeCh)
+
+	restEvent := &RestEvent{
+		Name: "testservice",
+		Type: "nodeup",
+		Host: "127.0.0.1",
+		Port: 9991,
+	}
+	b, err = json.Marshal(restEvent)
+	if err != nil {
+		t.Fatalf("Could not marshal test restevent: %s", string(b))
+	}
+	br := bytes.NewReader(b)
+	resp, err := http.Post(testserver.URL+"/service/testservice", "application/json", br)
+	if err != nil {
+		t.Fatalf("Error while Post: %s", err)
+	}
+	if resp.StatusCode != 200 {
+		t.Errorf("RestAPI send wrong statuscode: %d", resp.StatusCode)
+	}
+
+	// Check if event was fired
+	var recv event.Event
+	select {
+	case recv = <-eventCh:
+	case <-time.After(5 * time.Second):
+		t.Fatal("Timeout: No event received")
+	}
+
+	// Test if event is correct
+	if nlen := len(recv.Nodes()); nlen != 1 {
+		t.Errorf("Event has %d nodes, expected 1", nlen)
+	}
+	if evName := recv.Nodes()[0].Name(); evName != restEvent.Name {
+		t.Errorf("Event name was %s, expected %s", evName, restEvent.Name)
+	}
+	if evHost := recv.Nodes()[0].Host(); evHost != restEvent.Host {
+		t.Errorf("Event host was %s, expected %s", evHost, restEvent.Host)
+	}
+	if evPort := recv.Nodes()[0].Port(); evPort != restEvent.Port {
+		t.Errorf("Event port was %s, expected %s", evPort, restEvent.Port)
+	}
+	if evType := recv.Nodes()[0].Type(); evType != event.EventNodeUp {
+		t.Errorf("Event type was %s, expected %s", evType, event.EventNodeUp)
+	}
+
+}
