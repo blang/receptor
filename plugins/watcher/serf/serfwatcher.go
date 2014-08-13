bb45477955b39f4939e2b3daa5dba036cef4f63d
Benedikt Lang <github@benediktlang.de>
2014-08-13 18:03:13 +0200

    Serf watcher using bookkeeper

--- plugins/watcher/serf/serfwatcher.go
+++ plugins/watcher/serf/serfwatcher.go
@@ -15,8 +15,11 @@ type Watcher struct {
 }
 
 type ServiceConfig struct {
-	Addr    string `json:"addr"`
-	AuthKey string `json:"authkey"`
+	Addr    string            `json:"addr"`
+	AuthKey string            `json:"authkey"`
+	Tags    map[string]string `json:"tags"`
+	TagHost string            `json:"tag_host"`
+	TagPort string            `json:"tag_port"`
 }
 
 func (w *Watcher) Setup(cfgData json.RawMessage) error {
@@ -37,12 +40,12 @@ func (w *Watcher) Accept(cfgData json.RawMessage) (pipeline.Endpoint, error) {
 
 	return pipeline.EndpointFunc(func(eventCh chan pipeline.Event, closeCh chan struct{}) {
 		var client *serfc.RPCClient
-
+		_, fullEventCh := pipeline.Bookkeeper(eventCh)
 		defer func() {
 			if client != nil {
 				client.Close()
 			}
-			close(eventCh)
+			close(fullEventCh)
 		}()
 
 		for {
@@ -60,16 +63,26 @@ func (w *Watcher) Accept(cfgData json.RawMessage) (pipeline.Endpoint, error) {
 				members, err := client.Members()
 				if err != nil {
 					log.Printf("Error while fetching serf members: %s\n", err)
+					fullEventCh <- &pipeline.MultiNode{} // Mark all nodes as down
 					break
 				}
+
+				evt := &pipeline.MultiNode{}
 				for _, m := range members {
-					eventCh <- &pipeline.SingleNode{
+					if m.Status != "alive" || !matchTags(serviceCfg.Tags, m.Tags) {
+						continue
+					}
+					log.Printf("Node: %s:%d is %s\n", m.Addr.String(), m.Port, m.Status)
+
+					evt.Events = append(evt.Events, &pipeline.SingleNode{
 						EName: m.Name,
 						EType: pipeline.EventNodeUp,
 						EHost: m.Addr.String(),
 						EPort: int(m.Port),
-					}
+					})
 				}
+
+				fullEventCh <- evt
 
 				select {
 				case <-closeCh:
@@ -77,8 +90,23 @@ func (w *Watcher) Accept(cfgData json.RawMessage) (pipeline.Endpoint, error) {
 				case <-time.After(PULL_INTERVAL):
 				}
 			}
-
 		}
-
 	}), nil
+}
+
+func matchTags(required map[string]string, input map[string]string) bool {
+	if len(required) > 0 && input == nil {
+		return false
+	}
+	for key, value := range required {
+		if val2, found := input[key]; !found {
+			return false
+		} else {
+			if value == "*" || value == val2 {
+				continue
+			}
+			return false
+		}
+	}
+	return true
 }
